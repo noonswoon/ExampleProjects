3124714df17f9d1fa1a8c40aad1987b7f1c4c146
VatthanachaiW <vatthanachai.w@gmail.com>
2015-02-25 08:08:00 +0700

    Commit New Project for Use Azure Storage Table

---
+++ AzureTableStoragesDemo/WebDemo/Models/UsersEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace WebDemo.Models
+{
+    [DataContract]
+    public class UsersEntity : TableEntity
+    {
+        public UsersEntity(string firstname, string lastname)
+        {
+            this.PartitionKey = lastname;
+            this.RowKey = firstname;
+        }
+
+        public UsersEntity()
+        {
+        }
+
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
